80113bcdbffd27477d27b3095734d5126322a7cc
David Ko <davidsj.ko@gmail.com>
2023-07-17 09:46:09 -0400

    Make into FC and make active tabbing work

--- ankii-frontend/src/App.tsx
+++ ankii-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import styled from "styled-components";
 
@@ -16,14 +16,54 @@ const CardTextArea = styled.textarea`
   margin-bottom: 50px;
 `;
 
-function App() {
+const Container = styled.div`
+`;
+
+const CardViewControl = styled.div`
+  margin-top: 8px;
+  margin-left: 8px;
+`;
+
+interface Props {}
+
+const App: React.FC<Props> = (props) => {
+  const [frontMode, setFrontMode] = useState("input")
+
+  const inputMode = (setModeFn: React.Dispatch<React.SetStateAction<string>>) => {
+    setModeFn("input")
+  }
+
+  const previewMode = (setModeFn: React.Dispatch<React.SetStateAction<string>>) => {
+    setModeFn("preview")
+  }
+
   return (
     <div className="App">
       <FormContainer>
         <form>
           <div className="form-group">
-            <label>Front</label>
-            <CardTextArea className="form-control"></CardTextArea>
+            <label><h4>Front</h4></label>
+            <Container className="border rounded">
+              <CardViewControl className="border-bottom">
+                <ul className="nav nav-tabs">
+                  <li className="nav-item">
+                    <a
+                      className={"nav-link " + (frontMode == "input" ? "active" : "")}
+                      href="#"
+                      onClick={() => inputMode(setFrontMode)}
+                    >Input</a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className={"nav-link " + (frontMode == "preview" ? "active" : "")}
+                      href="#"
+                      onClick={() => previewMode(setFrontMode)}
+                    >Preview</a>
+                  </li>
+                </ul>
+              </CardViewControl>
+              <CardTextArea className="form-control"></CardTextArea>
+            </Container>
           </div>
           <div className="form-group">
             <label>Back</label>
